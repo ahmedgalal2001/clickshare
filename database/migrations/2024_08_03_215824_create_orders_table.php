0784addfeb8241dccfdc3e25072e640d749dece1
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-08-04 08:05:01 +0300

    feat: Create migration for orders table

---
+++ database/migrations/2024_08_03_215824_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_id')->unique();
+            $table->string('client_email');
+            $table->dateTime('order_date')->default(now());
+            $table->string('status');
+            $table->decimal('total', 10, 2);
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
