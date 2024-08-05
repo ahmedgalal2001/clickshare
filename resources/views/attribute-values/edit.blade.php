cbdccbe1708ede5548b6deead68be6ab38f22b9a
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-08-05 23:31:31 +0300

    feat: Add edit view for attribute values

---
+++ resources/views/attribute-values/edit.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Attribute Value') }}
+        </h2>
+    </x-slot>
+
+    <section class="mx-2 my-3">
+        <form class="max-w-sm mx-auto bg-white shadow p-4"
+            action="{{ route('attribute-values.update', $attributeValue->id) }}" method="POST">
+            @csrf
+            <div class="mb-5">
+                <label for="value" class="block mb-2 text-sm font-medium text-gray-900">Value</label>
+                <input type="text" id="value" name="value" value="{{ $attributeValue->value }}"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    placeholder="Value" required />
+            </div>
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Update
+                Attribute Value</button>
+        </form>
+    </section>
+</x-app-layout>
