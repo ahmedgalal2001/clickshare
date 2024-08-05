63ee1f61ffccca5aaa9394bfcc58eaa2106a3584
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-08-05 05:49:55 +0300

    chore: Add scheduled task to process orders hourly

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Services\GoogleSheetsServices;
+use App\Services\OrderProcessingService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    $googleSheets = new GoogleSheetsServices();
+    $processOrder =  new OrderProcessingService($googleSheets);
+    $processOrder->processOrders();
+})->hourly();
