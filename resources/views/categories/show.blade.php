d12cbcddd89376227d42076e09d1bea1a0b3299e
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-08-05 23:32:38 +0300

    feat: Add show view for categories

---
+++ resources/views/categories/show.blade.php
@@ -0,0 +1,7 @@
+<h1>{{ $category->name }}</h1>
+<a href="{{ route('categories.edit', $category->id) }}">Edit</a>
+<form action="{{ route('categories.destroy', $category->id) }}" method="POST" style="display:inline;">
+    @csrf
+    @method('DELETE')
+    <button type="submit">Delete</button>
+</form>
