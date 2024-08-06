5ce763bd30eb91333f622a33aa3e71261c6484a6
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-08-06 07:10:13 +0300

    refactor: Update categories show view for improved user experience

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,7 +1,45 @@
-<h1>{{ $category->name }}</h1>
-<a href="{{ route('categories.edit', $category->id) }}">Edit</a>
-<form action="{{ route('categories.destroy', $category->id) }}" method="POST" style="display:inline;">
-    @csrf
-    @method('DELETE')
-    <button type="submit">Delete</button>
-</form>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Category') }}
+        </h2>
+    </x-slot>
+
+    <section class="mx-2 my-3">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Categories Data</h5>
+        <div class="relative overflow-x-auto">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Attribute Value Value
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <td class="px-6 py-4">
+                            <a href="{{ route('categories.show', $category->id) }}">{{ $category->name }}</a>
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="{{ route('categories.edit', $category->id) }}">
+                                <i class="fa-solid fa-pen-to-square text-blue-500"></i>
+                            </a>
+                            <form action="{{ route('categories.destroy', $category->id) }}" method="POST"
+                                style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit">
+                                    <i class="fa-solid fa-trash text-red-600"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+</x-app-layout>
