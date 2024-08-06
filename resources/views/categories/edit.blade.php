a246b24d94f8acf0e6e1164f0e913e38509e2aa9
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-08-06 07:10:03 +0300

    refactor: Update edit view for categories to improve user experience

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,8 +1,23 @@
-<h1>Edit Category</h1>
-<form action="{{ route('categories.update', $category->id) }}" method="POST">
-    @csrf
-    @method('PATCH')
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" value="{{ $category->name }}">
-    <button type="submit">Update</button>
-</form>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Category') }}
+        </h2>
+    </x-slot>
+
+    <section class="mx-2 my-3">
+        <form class="max-w-sm mx-auto bg-white  shadow p-4" action="{{ route('categories.update', $category->id) }}"
+            method="POST">
+            @csrf
+            <div class="mb-5">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Name</label>
+                <input type="name" id="name" name="name" value="{{ $category->name }}"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    placeholder="Name" required />
+            </div>
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+                Update</button>
+        </form>
+    </section>
+</x-app-layout>
