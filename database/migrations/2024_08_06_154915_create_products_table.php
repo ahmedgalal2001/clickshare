625a3f3dcc91ae0ddad10e7c6da777c9ef806596
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-08-06 21:08:56 +0300

    refactor: Rename migration file for creating bundle_product table to create_products_table

--- database/migrations/2024_08_06_154915_create_products_table.php
+++ database/migrations/2024_08_06_154915_create_products_table.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('bundle_product', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('bundle_id')->constrained();
-            $table->foreignId('product_id')->constrained();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -24,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('bundle_product');
+        Schema::dropIfExists('products');
     }
 };
