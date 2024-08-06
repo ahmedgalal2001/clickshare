bd0a907cd316ccea3c5afc4461e2d533a6a2768a
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-08-06 07:10:08 +0300

    refactor: Update categories index view for improved user experience

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,15 +1,49 @@
-<h1>Categories</h1>
-<a href="{{ route('categories.create') }}">Create New Category</a>
-<ul>
-    @foreach ($categories as $category)
-        <li>
-            <a href="{{ route('categories.show', $category->id) }}">{{ $category->name }}</a>
-            <a href="{{ route('categories.edit', $category->id) }}">Edit</a>
-            <form action="{{ route('categories.destroy', $category->id) }}" method="POST" style="display:inline;">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Delete</button>
-            </form>
-        </li>
-    @endforeach
-</ul>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Categories') }}
+        </h2>
+    </x-slot>
+
+
+    @include('components.flatButton', ['route' => 'categories.create'])
+    <section class="mx-2 my-3">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Categories Data</h5>
+        <div class="relative overflow-x-auto">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Attribute Value Value
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $category)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td class="px-6 py-4">
+                                <a href="{{ route('categories.show', $category->id) }}">{{ $category->name }}</a>
+                            </td>
+                            <td class="px-6 py-4">
+                                <a href="{{ route('categories.edit', $category->id) }}">
+                                    <i class="fa-solid fa-pen-to-square text-blue-500"></i>
+                                </a>
+                                <form action="{{ route('categories.destroy', $category->id) }}" method="POST"
+                                    style="display:inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit">
+                                        <i class="fa-solid fa-trash text-red-600"></i>
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </section>
+</x-app-layout>
